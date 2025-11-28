636d4acf73610d3d0ebc7cafe1988cc2e26102e7
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-11-28 11:30:55 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- SONALI/utils/inline/queue.py
+++ SONALI/utils/inline/queue.py
@@ -68,16 +68,10 @@ def aq_markup(_, chat_id):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(
-                text="𝖠ʟᴘʜᴀ 𝖶ᴏʀʟᴅ 🥀", url=f"https://t.me/WORLD_ALPHA"
-            ),
-            InlineKeyboardButton(
-                text="𝖢ʜᴀᴛ 𝖦ʀᴏᴜᴘ 🥀", url=f"https://t.me/PURVI_UPDATES"
-            ),
-        ],
-      ]
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
     return buttons
