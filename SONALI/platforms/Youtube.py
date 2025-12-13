19b0fe8be45ce5f615417359aeb4016aee1885d8
RockMusicBot <githubmusicbot@gmail.com>
2025-12-13 17:34:45 +0530

    Update and rename toxic.py to Youtube.py

--- SONALI/platforms/Youtube.py
+++ SONALI/platforms/Youtube.py
@@ -15,9 +15,9 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from youtubesearchpython.__future__ import VideosSearch, CustomSearch
 import base64
-from SONALI_MUSIC import LOGGER
-from SONALI_MUSIC.utils.database import is_on_off
-from SONALI_MUSIC.utils.formatters import time_to_seconds
+from SONALI import LOGGER
+from SONALI.utils.database import is_on_off
+from SONALI.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
 logger = LOGGER(__name__)
